13724a9de436bb0fec75fa12d109cc774961cc2e
moira <moira.sanna@gmail.com>
2019-02-13 12:05:09 +0000

    Added actions, view files and a simple html form for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,42 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+end
+
   def new
+  	@user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+end
+
+def edit
+    @user = User.find(params[:id])
+end
+
   def create
+  	@user = User.new(user_params)
+
+  	if @user.save
+  		redirect_to users_path(@user)
+  	else
+  		render :new
+  	end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+end
+  
+  private
+  def user_params
+  	params.require(:user).permit(:username, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root to: 'users#index'
 
-	resources :user, :only => [:new, :create]
+  get 'new', to: 'users#new'
+
+  get 'users/create'
+  
+  get 'index', to: 'users#index'
+
+  get 'users/show'
+
+  get 'users/edit'
+
+	resources :users
 end
